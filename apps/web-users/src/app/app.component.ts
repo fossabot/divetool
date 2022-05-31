c65e17c57fae20185532848cfef84e373f6be204
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2022-05-31 10:06:39 -0500

    chore(repo): initial commit

---
+++ apps/web-users/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'divetool-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web-users';
+}
